fbec77115557b46cff56cd6a57056bfcbfc0cb29
Rolem Galvez <rolemgalvez@gmail.com>
2022-12-13 15:32:48 +0800

    feature: add 13thMonthPay program

---
+++ 13thMonthPay/Program.cs
@@ -0,0 +1,49 @@
+﻿//Welcome message
+Console.WriteLine("13th Month Pay");
+Console.WriteLine("--------------");
+
+bool runAgain = false;
+
+do
+{
+    runAgain = false;
+    Console.WriteLine();
+
+    //Get user inputs
+    Console.Write("Enter your monthly basic salary: ");
+    bool isValidSalary = decimal.TryParse(Console.ReadLine(), out decimal salary);
+
+    Console.Write("In a year, how many months you have worked? ");
+    bool isValidMonth = int.TryParse(Console.ReadLine(), out int monthsWorked);
+
+    bool areValidInputs = isValidSalary && isValidMonth;
+
+    //Process results
+    if (areValidInputs == false)
+    {
+        Console.WriteLine();
+        Console.WriteLine("You've entered an invalid input. Try again.");
+        runAgain = true;
+    }
+
+    if (areValidInputs)
+    {
+        Console.WriteLine();
+        decimal thirteenthMonthPay = (salary / 12) * monthsWorked;
+        Console.WriteLine($"Your 13th month pay is {thirteenthMonthPay.ToString("N")}");
+        
+        //Ask to run again
+        Console.WriteLine();
+        Console.Write("Enter \"yes\" to run again: ");
+        string? answer = Console.ReadLine();
+
+        if (answer?.ToLower() == "yes")
+        {
+            runAgain = true;
+        }
+    }
+} while (runAgain);
+
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
