d53a95a8bbb4ca6ed13604c7b462979d2b699952
Rolem Galvez <rolemgalvez@gmail.com>
2022-12-14 10:48:10 +0800

    feature: add OrdinaryOvertimePayment project

--- OrdinaryOvertimePayment/Program.cs
+++ OrdinaryOvertimePayment/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Welcome message
-Console.WriteLine("Daily Rate");
-Console.WriteLine("----------");
+Console.WriteLine("Ordinary Overtime Payment");
+Console.WriteLine("-------------------------");
 
 bool runAgain = false;
 do
@@ -9,13 +9,14 @@ do
     Console.WriteLine();
 
     //Get user input
+    Console.WriteLine("Considering 261 is your factor rate and 8 hours work per day...");
     Console.Write("Enter monthly basic salary: ");
     bool isValidSalary = decimal.TryParse(Console.ReadLine(), out decimal salary);
 
-    Console.Write("What factor rate are you using (days of work per year)? ");
-    bool isValidFactorRate = decimal.TryParse(Console.ReadLine(), out decimal factorRate);
+    Console.Write("How many overtime minutes you've worked? ");
+    bool isValidOvertime = decimal.TryParse(Console.ReadLine(), out decimal overtime);
 
-    bool areValidInputs = isValidSalary && isValidFactorRate;
+    bool areValidInputs = isValidSalary && isValidOvertime;
 
     //Process results
     if (areValidInputs == false)
@@ -27,12 +28,13 @@ do
 
     if (areValidInputs)
     {
+        decimal hourlyRate = (salary * 12) / 261 / 8;
+        decimal payment = hourlyRate * 1.25M * (overtime/60);
         Console.WriteLine();
-        decimal dailyRate = (salary * 12) / factorRate;
-        Console.WriteLine($"Your daily rate is {dailyRate.ToString("N")}.");
+        Console.WriteLine($"Your overtime payment is {payment.ToString("N")}.");
 
         Console.WriteLine();
-        Console.Write("Enter \"yes\" to do computation again: ");
+        Console.Write("Enter \"yes\" to compute again: ");
         runAgain = Console.ReadLine()?.ToLower() == "yes";
     }
 } while (runAgain);
